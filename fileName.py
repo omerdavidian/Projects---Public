e9e6bf7cce078d033c86b4f2732a35a258277919
Omz <78838816+omerdavidian@users.noreply.github.com>
2024-08-24 14:35:56 -0600

    Add files via upload

---
+++ fileName.py
@@ -0,0 +1,29 @@
+import os
+
+def rename_files_in_directory(directory):
+    
+    # Run through all the files in a directory and check for the folloing criteria
+    for filename in os.listdir(directory):
+        
+        #remove an ending of a file name "Photo1-Enhanced-NR-Edit.jpeg" --> "Photo1.jpeg"
+        if '-Enhanced-NR-Edit' in filename:
+            new_name = filename.replace('-Enhanced-NR-Edit', '')
+            old_file_path = os.path.join(directory, filename)
+            new_file_path = os.path.join(directory, new_name)
+            os.rename(old_file_path, new_file_path)
+            print(f'Renamed: {filename} -> {new_name}')   
+        
+        #remove an ending of a file name "Photo1-Enhanced-NR.jpeg" --> "Photo1.jpeg"         
+        elif '-Enhanced-NR' in filename:
+            new_name = filename.replace('-Enhanced-NR', '')
+            old_file_path = os.path.join(directory, filename)
+            new_file_path = os.path.join(directory, new_name)
+            os.rename(old_file_path, new_file_path)
+            print(f'Renamed: {filename} -> {new_name}')
+        else:
+            pass
+
+# Directory path
+directory_path = r"C:\Folder Location..."
+
+rename_files_in_directory(directory_path)
