951e11eb61cd3303c8284c2f7d72539217b013ad
Omz <78838816+omerdavidian@users.noreply.github.com>
2024-08-24 18:46:18 -0600

    test

--- fileName.py
+++ fileName.py
@@ -5,17 +5,17 @@ def rename_files_in_directory(directory):
     # Run through all the files in a directory and check for the folloing criteria
     for filename in os.listdir(directory):
         
-        #remove an ending of a file name "Photo1-Enhanced-NR-Edit.jpeg" --> "Photo1.jpeg"
+        # Remove an ending of a file name "Photo1-Enhanced-NR-Edit.jpeg" --> "Photo1.jpeg"
         if '-Enhanced-NR-Edit' in filename:
-            new_name = filename.replace('-Enhanced-NR-Edit', '')
+            new_name = filename.replace('-Enhanced-NR-Edit', '') # Change text as needed
             old_file_path = os.path.join(directory, filename)
             new_file_path = os.path.join(directory, new_name)
             os.rename(old_file_path, new_file_path)
             print(f'Renamed: {filename} -> {new_name}')   
         
-        #remove an ending of a file name "Photo1-Enhanced-NR.jpeg" --> "Photo1.jpeg"         
+        # Remove an ending of a file name "Photo1-Enhanced-NR.jpeg" --> "Photo1.jpeg"         
         elif '-Enhanced-NR' in filename:
-            new_name = filename.replace('-Enhanced-NR', '')
+            new_name = filename.replace('-Enhanced-NR', '') # Change text as needed
             old_file_path = os.path.join(directory, filename)
             new_file_path = os.path.join(directory, new_name)
             os.rename(old_file_path, new_file_path)
@@ -27,3 +27,6 @@ def rename_files_in_directory(directory):
 directory_path = r"C:\Folder Location..."
 
 rename_files_in_directory(directory_path)
+
+# test 1
+
